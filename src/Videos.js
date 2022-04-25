18f5dfe30de15ba0925dd7341c0a03a7bcdbc390
John <jpilik@fream.pl>
2022-04-25 10:40:15 +0200

    localstorage, fixed saving server response via JSON

--- src/Videos.js
+++ src/Videos.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Video } from "./Video";
 
 function Videos() {
@@ -7,22 +6,92 @@ function Videos() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyBNujw7CvH2IEOZ0XavXA87nyA7MBmgsT4"
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
+  const LOCAL_STORAGE_LATEST_QUERY = "LOCAL_STORAGE_LATEST_QUERY";
+  localStorage.setItem(
+    LOCAL_STORAGE_LATEST_QUERY,
+    JSON.stringify({
+      kind: "youtube#searchListResponse",
+      etag: "qEHiHVc1QUrf3QdiYgEYiDJlOjY",
+      nextPageToken: "CAUQAA",
+      regionCode: "PL",
+      pageInfo: {
+        totalResults: 1000000,
+        resultsPerPage: 5,
+      },
+      items: [
+        {
+          kind: "youtube#searchResult",
+          etag: "nFGBxnyxkqvPkmVe5AgjtF2Ufgo",
+          id: {
+            kind: "youtube#video",
+            videoId: "1qrhnK6FGr0",
+          },
         },
+        {
+          kind: "youtube#searchResult",
+          etag: "ZXjMy1uj95g-ttAOgAPdyjtM6ss",
+          id: {
+            kind: "youtube#video",
+            videoId: "hxhj2PFExMg",
+          },
+        },
+        {
+          kind: "youtube#searchResult",
+          etag: "K1S16yOeVtUIzZt3ZqB1jAo1LIs",
+          id: {
+            kind: "youtube#video",
+            videoId: "OmKbGOARXao",
+          },
+        },
+        {
+          kind: "youtube#searchResult",
+          etag: "8rRBWAeI0BI4nXtmWA_L2d-FDy8",
+          id: {
+            kind: "youtube#video",
+            videoId: "rZCEBibnRM8",
+          },
+        },
+        {
+          kind: "youtube#searchResult",
+          etag: "3xR1nuNhxJ0oLJoKGiKMz_5dXY4",
+          id: {
+            kind: "youtube#video",
+            videoId: "XcDCYb0gt48",
+          },
+        },
+      ],
+    })
+  );
+  const savedItem = localStorage.getItem(LOCAL_STORAGE_LATEST_QUERY);
+  console.log(JSON.parse(savedItem));
 
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+  useEffect(() => {
+    if (!savedItem) {
+      fetch(
+        "https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyCeQZ_MjmHNlaK67QI5thMNWGhQQHvTK48"
+      )
+        .then((res) => res)
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            console.log("ALERT ANOTHER REQUEST!!!!");
+            localStorage.setItem(LOCAL_STORAGE_LATEST_QUERY, result);
+
+            setItems(result);
+          },
+
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+    }
+    if (savedItem) {
+      console.log("RUNNING FROM LOCAL STORAGE INSTEAD OF FETCH");
+      console.log(savedItem);
+      setItems(savedItem);
+      setIsLoaded(true);
+    }
   }, []);
 
   if (error) {
@@ -37,7 +106,13 @@ function Videos() {
             {item.name} {item.price}
           </li>
         ))} */}
-        <Video />
+        <>
+          {items.items.map((item, i) => {
+            // console.log(items);
+            return <div key={item.id.videoId}>{item.id.videoId}</div>;
+          })}
+          <Video />
+        </>
       </ul>
     );
   }
