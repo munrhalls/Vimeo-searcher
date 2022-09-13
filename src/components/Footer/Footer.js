6e44773d7cbccf29b03c664799999b617b6a6b40
John <antarcticdepths@gmail.com>
2022-09-13 09:32:16 +0200

    vimeo

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="Footer__attribs">
+        <a
+          className="Footer__attribs__link"
+          target="_blank"
+          href="https://www.flaticon.com/free-icons/pirate-ship"
+          title="pirate ship icons"
+        >
+          Pirate ship icons created by Amethyst prime - Flaticon
+        </a>
+      </div>
+    </div>
+  );
+}
