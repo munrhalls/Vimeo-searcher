ce51a18d26b802c38af388005f1ffa42506bd0d1
John <antarcticdepths@gmail.com>
2022-09-13 16:00:13 +0200

    make cool af loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,14 +1,25 @@
 import React from "react";
 
 export function Loader({ children, active }) {
+  let decrement = [70, 60, 50, 40, 30, 20, 10];
   return (
     <>
       {active ? (
         <div className="Loader">
           {Array(7)
             .fill(null)
-            .map((el) => {
-              return <div className="Loader__symbol"></div>;
+            .map((el, index) => {
+              return (
+                <div
+                  style={{
+                    height: `${decrement[index]}%`,
+                    width: `${decrement[index]}%`,
+                  }}
+                  className={`Loader__symbol ${
+                    index % 2 === 0 ? "--counterRotate" : "--rotate"
+                  }`}
+                ></div>
+              );
             })}
         </div>
       ) : (
