85331d9de732ca44ab2dc1ee6503c923f5d8378c
John <antarcticdepths@gmail.com>
2022-09-13 10:46:00 +0200

    vimeo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,13 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="Footer">
+      <div className="Footer__authorAttrib">
+        <h3 className="Footer__authorAttrib__metaTitle">
+          DESIGN, APP, WEBSITE BY:
+        </h3>
+        <h3 className="Footer__authorAttrib__title">MUNRHALLS 2022.</h3>
+      </div>
+
       <div className="Footer__attribs">
         <h3 className="Footer__attribs__metaTitle">ICONS BY:</h3>
         <div className="Footer__attribs__container">
