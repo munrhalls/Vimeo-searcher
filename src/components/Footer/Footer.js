34db1257d79fcfdc9ae0ffccfbc9c5460d21d8d4
John <antarcticdepths@gmail.com>
2022-09-13 10:34:42 +0200

    footer shows

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,17 @@ export default function Footer() {
   return (
     <div className="Footer">
       <div className="Footer__attribs">
-        <a
-          className="Footer__attribs__link"
-          target="_blank"
-          href="https://www.flaticon.com/free-icons/pirate-ship"
-          title="pirate ship icons"
-        >
-          Pirate ship icons created by Amethyst prime - Flaticon
-        </a>
+        <h3 className="Footer__attribs__metaTitle">ICONS BY:</h3>
+        <div className="Footer__attribs__container">
+          <a
+            className="Footer__attribs__container__link"
+            target="_blank"
+            href="https://www.flaticon.com/free-icons/pirate-ship"
+            title="pirate ship icons"
+          >
+            Pirate ship icons created by Amethyst prime - Flaticon
+          </a>
+        </div>
       </div>
     </div>
   );
