61dd266bb787c38242899a266155b254646fc540
John <antarcticdepths@gmail.com>
2022-09-13 10:55:00 +0200

    vimeo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ManageAccount } from "../ManageAccount/ManageAccount";
 import { useGlobal } from "../Contexts/GlobalProvider";
+import IMG__SAILINGSHIP from "./../../Assets/sailing-ship.png";
 
 export default function Header() {
   const { currentUser, setCurrentUser } = useGlobal();
@@ -8,6 +9,11 @@ export default function Header() {
   return (
     <div className="Header">
       <h1 className="Header__title"> VIMEO SEARCHER</h1>
+      <img
+        className="Header__icon"
+        src={IMG__SAILINGSHIP}
+        alt="SAILING SHIP ICON"
+      />
       {/* <ManageAccount loggedUser={currentUser} setLoggedUser={setCurrentUser} /> */}
     </div>
   );
