738e1aa9ba4e01d381dd61e2552894d752615db1
John <antarcticdepths@gmail.com>
2022-09-14 10:25:45 +0200

    loader & videos work now

--- src/components/DisplayManager/DisplayManager.js
+++ src/components/DisplayManager/DisplayManager.js
@@ -2,5 +2,6 @@ import React from "react";
 import Videos from "./Videos";
 
 export const DisplayManager = ({ searchVideos }) => {
+  // open for extension, closed for modification
   return <Videos videos={searchVideos} />;
 };
